a7ef1cb713191cfdc73d6d145eca7dc106c05ccf
mehmetyasincirakpy <myasincirak@gmail.com>
2023-04-29 15:37:32 +0300

    FLO RFM
    Signed-off-by: mehmetyasincirakpy <myasincirak@gmail.com>

--- FLO_RFM.py
+++ FLO_RFM.py
@@ -41,8 +41,7 @@ dataframe.sort_values("order_num_total", ascending=False)[:10]
 
 def data_prep(dataframee):
     dataframe["order_num_total"] = dataframe["order_num_total_ever_online"] + dataframe["order_num_total_ever_offline"]
-    dataframe["customer_value_total"] = dataframe["customer_value_total_ever_offline"] + dataframe[
-        "customer_value_total_ever_online"]
+    dataframe["customer_value_total"] = dataframe["customer_value_total_ever_offline"] + dataframe["customer_value_total_ever_online"]
     date_columns = dataframe.columns[dataframe.columns.str.contains("date")]
     dataframe[date_columns] = dataframe[date_columns].apply(pandas.to_datetime)
     return dataframe
@@ -88,13 +87,57 @@ rfm.head()
 rfm[["segment", "recency", "frequency", "monetary"]].groupby("segment").agg(["mean", "count"])
 
 target_segments_customer_ids = rfm[rfm["segment"].isin(["champions", "loyal_customers"])]["customer_id"]
-customers_ids = dataframe[(dataframe["master_id"].isin(target_segments_customer_ids)) & (
-    dataframe["interested_in_categories_12"].str.contains("KADIN"))]["master_id"]
+customers_ids = dataframe[(dataframe["master_id"].isin(target_segments_customer_ids)) & (dataframe["interested_in_categories_12"].str.contains("KADIN"))]["master_id"]
 customers_ids.to_csv("yeni_marka_hedef_musteri_id.csv", index=False)
 
 target_segments_customer_ids = rfm[rfm["segment"].isin(["cant_loose", "atrisk", "new_customers"])]["customer_id"]
 cust_ids = dataframe[(dataframe["master_id"].isin(target_segments_customer_ids)) & (
-            (dataframe["interested_in_categories_12"].str.contains("ERKEK")) | (
-        dataframe["interested_in_categories_12"].str.contains("COCUK")))]["master_id"]
+        (dataframe["interested_in_categories_12"].str.contains("ERKEK")) | (dataframe["interested_in_categories_12"].str.contains("COCUK")))]["master_id"]
 
 cust_ids.to_csv("indirim_hedef_müşteri_ids.csv", index=False)
+
+
+def create_rfm(dataframee):
+    # Veriyi hazırlama
+    dataframe["order_num_total"] = dataframe["order_num_total_ever_online"] + dataframe["order_num_total_ever_offline"]
+    dataframe["customer_value_total"] = dataframe["customer_value_total_ever_offline"] + dataframe["customer_value_total_ever_online"]
+    date_columns = dataframe.columns[dataframe.columns.str.contains("date")]
+    dataframe[date_columns] = dataframe[date_columns].apply(pandas.to_datetime)
+
+    # RFM metriklerinin hesaplanması
+    dataframe["last_order_date"].max()
+    analysis_date = datetime.datetime(2021, 6, 1)
+
+    rfm = pandas.DataFrame()
+    rfm["customer_id"] = dataframe["master_id"]
+    rfm["recency"] = (analysis_date - dataframe["last_order_date"]).astype('timedelta64[D]')
+    rfm["frequency"] = dataframe["order_num_total"]
+    rfm["monetary"] = dataframe["customer_value_total"]
+
+    rfm["recency_score"] = pandas.qcut(rfm["recency"], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pandas.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+    rfm["monetary_score"] = pandas.qcut(rfm["monetary"], 5, labels=[1, 2, 3, 4, 5])
+
+    rfm["RF_SCORE"] = (rfm["recency_score"].astype(str) + rfm["frequency_score"].astype(str))
+    rfm["RFM_SCORE"] = (rfm["recency_score"].astype(str) + rfm["frequency_score"].astype(str) + rfm["monetary_score"].astype(str))
+
+    # Segmentlerin oluşturulması
+    seg_map = {
+        r'[1-2][1-2]': 'hibernating',
+        r'[1-2][3-4]': 'at_Risk',
+        r'[1-2]5': 'cant_loose',
+        r'3[1-2]': 'about_to_sleep',
+        r'33': 'need_attention',
+        r'[3-4][4-5]': 'loyal_customers',
+        r'41': 'promising',
+        r'51': 'new_customers',
+        r'[4-5][2-3]': 'potential_loyalists',
+        r'5[4-5]': 'champions'
+    }
+    rfm["segment"] = rfm["RF_SCORE"].replace(seg_map, regex=True)
+
+    return rfm[["customer_id", "recency", "frequency", "monetary", "RF_SCORE", "RFM_SCORE", "segment"]]
+
+
+rfm_dataframe = create_rfm(dataframe)
+rfm_dataframe.head()
